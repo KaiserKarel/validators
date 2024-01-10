4f626bc8adc497c7ace41b66998d8838ec1f462d
karel <k.l.kubat@gmail.com>
2024-01-10 22:38:58 +0000

    save

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
-wakey:
-	GIT_LFS_SKIP_SMUDGE=1 nixos-rebuild --target-host wakey --use-remote-sudo switch --flake ./#wakey
 
-wakey-rpc:
-	GIT_LFS_SKIP_SMUDGE=1 nixos-rebuild --target-host wakey-rpc --use-remote-sudo switch --flake ./#wakey-rpc
-	
+kaiserkarel:
+	GIT_LFS_SKIP_SMUDGE=1 nixos-rebuild --target-host kaiserkarel --use-remote-sudo switch --flake ./#kaiserkarel
--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    union.url = "git+ssh://git@github.com/unionlabs/union";
+    union.url = "github:unionlabs/union";
     sops-nix.url = "github:Mic92/sops-nix";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, union, sops-nix, ... }:
+  outputs = { self, nixpkgs, union, sops-nix, crane, ... }:
     {
-      nixosConfigurations.wakey-rpc =
+      nixosConfigurations.kaiserkarel =
         let
           system = "x86_64-linux";
           pkgs = import nixpkgs { inherit system; };
@@ -14,67 +18,28 @@
         nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
-            union.nixosModules.unionvisor
-            union.nixosModules.hubble
             sops-nix.nixosModules.sops
+            union.nixosModules.unionvisor
             "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
             {
               system.stateVersion = "23.11";
 
-              sops = {
-                age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-                secrets = {
-                  datadog_api_key = {
-                    restartUnits = [ "datadog-agent.service" ];
-                    path = "/etc/datadog-agent/datadog_api.key";
-                    sopsFile = ./secrets/datadog.yaml;
-                    mode = "0440";
-                    owner = "datadog";
-                  };
-                  priv_validator_key = {
-                    restartUnits = [ "unionvisor.service" ];
-                    format = "binary";
-                    sopsFile = ./secrets/wakey-rpc/priv_validator_key.json;
-                    path = "/var/lib/unionvisor/home/config/priv_validator_key.json";
-                  };
-                  hasura-admin-secret = {
-                    restartUnits = [ "hubble.service" ];
-                    path = "/etc/hubble/hubble.key";
-                    sopsFile = ./secrets/wakey-rpc/hubble.yaml;
-                  };
-                };
-              };
-
-              networking.firewall.allowedTCPPorts = [ 80 443 26656 26657 ];
+              networking.firewall.allowedTCPPorts = [ 80 443 26656 26657 26666 ];
+              networking.hostName = "kaiserkarel";
 
               services.unionvisor = {
                 enable = true;
-                moniker = "wakey-wakey-rpc";
-              };
-
-              services.hubble = {
-                enable = true;
-                url = "https://noble-pika-27.hasura.app/v1/graphql";
-                api-key-file = "/etc/hubble/hubble.key";
-                indexers = [
-                  { url = "http://localhost:26657"; type = "tendermint"; }
-                ];
-                metrics-addr = "0.0.0.0:9091";
-              };
-
-              nix = {
-                settings = { auto-optimise-store = true; };
-                gc = {
-                  automatic = true;
-                  dates = "weekly";
-                  options = "--delete-older-than 15d";
-                };
+                moniker = "kaiserkarel";
+                seeds = "a069a341154484298156a56ace42b6e6a71e7b9d@blazingbit.io:27656";
               };
 
               users.users.datadog.extraGroups = [ "systemd-journal" ];
 
               services.datadog-agent = {
                 enable = true;
+                package = pkgs.datadog-agent.override {
+                  extraTags = [ "docker" ];
+                };
                 apiKeyFile = "/etc/datadog-agent/datadog_api.key";
                 enableLiveProcessCollection = true;
                 enableTraceAgent = true;
@@ -93,59 +58,15 @@
                           ".*"
                         ];
                       }
-                      {
-                        openmetrics_endpoint = "http://localhost:9091/metrics";
-                        namespace = "hubble";
-                        metrics = [
-                          ".*"
-                        ];
-                        max_returned_metrics = 200000;
-                      }
                     ];
                   };
                 };
               };
 
-              environment.systemPackages = with pkgs; [
-                bat
-                bottom
-                helix
-                jq
-                neofetch
-                tree
+              users.users.root.openssh.authorizedKeys.keys = [
+                ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmoB0Q5eqWmTgXnh4PU8G9J5Rq9OXaNjf5nmbXS5nhv''
+                ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAY6rYPYLUl8ccZsZUUZgTyqpwp0CUIYsBhy+4Ub/UuB''
               ];
-            }
-          ];
-        };
-      nixosConfigurations.wakey =
-        let
-          system = "x86_64-linux";
-          pkgs = import nixpkgs { inherit system; };
-        in
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            union.nixosModules.unionvisor
-            sops-nix.nixosModules.sops
-            "${nixpkgs}/nixos/modules/virtualisation/openstack-config.nix"
-            {
-              system.stateVersion = "23.11";
-
-              networking.firewall.allowedTCPPorts = [ 80 443 26656 26657 ];
-
-              services.unionvisor = {
-                enable = true;
-                moniker = "wakey-wakey";
-              };
-
-              nix = {
-                settings = { auto-optimise-store = true; };
-                gc = {
-                  automatic = true;
-                  dates = "weekly";
-                  options = "--delete-older-than 15d";
-                };
-              };
 
               sops = {
                 age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
@@ -160,34 +81,24 @@
                   priv_validator_key = {
                     restartUnits = [ "unionvisor.service" ];
                     format = "binary";
-                    sopsFile = ./secrets/wakey/priv_validator_key.json;
+                    sopsFile = ./secrets/kaiserkarel/priv_validator_key.json;
                     path = "/var/lib/unionvisor/home/config/priv_validator_key.json";
+                  };
+                  hubble-key = {
+                    restartUnits = [ "hubble.service" ];
+                    path = "/etc/hubble/hubble.key";
+                    sopsFile = ./secrets/kaiserkarel/hubble.key;
+                    format = "binary";
                   };
                 };
               };
 
-              users.users.datadog.extraGroups = [ "systemd-journal" ];
-
-              services.datadog-agent = {
-                enable = true;
-                apiKeyFile = "/etc/datadog-agent/datadog_api.key";
-                enableLiveProcessCollection = true;
-                enableTraceAgent = true;
-                site = "datadoghq.eu";
-                extraIntegrations = { openmetrics = _: [ ]; };
-                extraConfig = { logs_enabled = true; };
-                checks = {
-                  journald = { logs = [{ type = "journald"; }]; };
-                  openmetrics = {
-                    init_configs = { };
-                    instances = [{
-                      openmetrics_endpoint = "http://localhost:26660/metrics";
-                      namespace = "cometbft";
-                      metrics = [
-                        ".*"
-                      ];
-                    }];
-                  };
+              nix = {
+                settings = { auto-optimise-store = true; };
+                gc = {
+                  automatic = true;
+                  dates = "weekly";
+                  options = "--delete-older-than 15d";
                 };
               };
 
